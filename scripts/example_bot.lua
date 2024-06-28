b8c2eabee2b63d2a63b965c6d1f5a26bdbdfbe14
Timothy Quilling <teqed@shatteredsky.net>
2024-06-28 16:11:16 -0400

    Cleanup Mr. Prog message

--- scripts/example_bot.lua
+++ scripts/example_bot.lua
@@ -31,16 +31,14 @@ Net:on("actor_interaction", function(event)
 
   Net.set_bot_direction(bot_id, Direction.from_points(bot_pos, Net.get_player_position(player_id)))
 
-  -- Async.message_player(player_id, "HELLO! WELCOME TO TEQ'S WEBPAGE!", mug_texture_path, mug_animation_path).and_then(function()
-  --   Net.unlock_player_input(player_id)
-  --   Net.set_bot_direction(bot_id, Direction.DOWN_LEFT)
-  -- end)
-
   local temperature = Net.get_system_temperature()
+  temperature = string.format("%.2f", temperature)
 
-  Async.message_player(player_id, "HELLO! THE CPU TEMPERATURE IS " .. temperature .. " CELSIUS!", mug_texture_path, mug_animation_path).and_then(function()
-    Net.unlock_player_input(player_id)
-    Net.set_bot_direction(bot_id, Direction.DOWN_LEFT)
+  Async.message_player(player_id, "HELLO! WELCOME TO TEQ'S WEBPAGE!", mug_texture_path, mug_animation_path).and_then(function()
+    Async.message_player(player_id, "THE CPU TEMPERATURE IS " .. temperature .. " CELSIUS!", mug_texture_path, mug_animation_path).and_then(function()
+      Net.unlock_player_input(player_id)
+      Net.set_bot_direction(bot_id, Direction.DOWN_LEFT)
+    end)
   end)
 
   -- Async.question_player(player_id, "HELLO! ARE YOU DOING WELL TODAY?", mug_texture_path, mug_animation_path)

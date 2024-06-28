f7a508d212acbcc798c833d0c2c53f27717992d9
Timothy Quilling <teqed@shatteredsky.net>
2024-06-28 16:29:33 -0400

    Cleanup Mr. Prog message

--- scripts/example_bot.lua
+++ scripts/example_bot.lua
@@ -37,14 +37,12 @@ Net:on("actor_interaction", function(event)
   system_load = string.format("%.2f", system_load)
 
   Async.message_player(player_id, "HELLO! WELCOME TO TEQ'S WEBPAGE! \
-       \
-      THE CPU TEMPERATURE IS " .. temperature .. " CELSIUS! \
-       \
-      THE SYSTEM LOAD IS " .. system_load .. "%!", mug_texture_path, mug_animation_path).and_then(function()
-    Async.message_player(player_id, "", mug_texture_path, mug_animation_path).and_then(function()
-      Net.unlock_player_input(player_id)
-      Net.set_bot_direction(bot_id, Direction.DOWN_LEFT)
-    end)
+\
+THE CPU TEMPERATURE IS " .. temperature .. " CELSIUS! \
+\
+THE SYSTEM LOAD IS " .. system_load .. "%!", mug_texture_path, mug_animation_path).and_then(function()
+    Net.unlock_player_input(player_id)
+    Net.set_bot_direction(bot_id, Direction.DOWN_LEFT)
   end)
 
   -- Async.question_player(player_id, "HELLO! ARE YOU DOING WELL TODAY?", mug_texture_path, mug_animation_path)
